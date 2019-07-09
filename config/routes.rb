cc621980b8ddaa3f0687067ac5857de4e4b292c0
Zuzanna Pusiewicz <zuzpusie@student.pg.gda.pl>
2019-07-09 22:11:32 +0200

    set docs belong to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'welcome#index'
   resources :docs
+
+  authenticated :user do 
+    root 'docs#index', as: 'authenticated_root'
+  end
+
+  root 'welcome#index'
 end
